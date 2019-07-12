451a16a14698766f7a652e41a376984dd913f19c
konradkrakowiecki <konrad.krakowiecki@gmail.com>
2019-07-13 00:06:06 +0200

    Added Create a Recipe.

--- src/main/java/pl/iamkonradkrakowiecki/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/pl/iamkonradkrakowiecki/spring5recipeapp/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package pl.iamkonradkrakowiecki.spring5recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.iamkonradkrakowiecki.spring5recipeapp.commands.RecipeCommand;
 import pl.iamkonradkrakowiecki.spring5recipeapp.services.RecipeService;
 
 @Controller
@@ -19,5 +22,20 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
