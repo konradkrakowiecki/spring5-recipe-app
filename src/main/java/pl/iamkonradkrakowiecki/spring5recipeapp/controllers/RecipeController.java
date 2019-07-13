9ce0f46cbcc02c5c1fe2b7d5b193e108b877406c
konradkrakowiecki <konrad.krakowiecki@gmail.com>
2019-07-14 00:08:41 +0200

    Added Delete a Recipe.

--- src/main/java/pl/iamkonradkrakowiecki/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/pl/iamkonradkrakowiecki/spring5recipeapp/controllers/RecipeController.java
@@ -2,10 +2,7 @@ package pl.iamkonradkrakowiecki.spring5recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.iamkonradkrakowiecki.spring5recipeapp.commands.RecipeCommand;
 import pl.iamkonradkrakowiecki.spring5recipeapp.services.RecipeService;
 
@@ -18,6 +15,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
@@ -25,6 +23,7 @@ public class RecipeController {
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping("/recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
@@ -32,6 +31,7 @@ public class RecipeController {
         return "recipe/recipeform";
     }
 
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
@@ -45,5 +45,15 @@ public class RecipeController {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show/";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id) {
+//        log.debug("Deleting id: " + id);
+
+        recipeService.deleteById(Long.valueOf(id));
+
+        return "redirect:/";
     }
 }
--- src/test/java/pl/iamkonradkrakowiecki/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/pl/iamkonradkrakowiecki/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -3,6 +3,7 @@ package pl.iamkonradkrakowiecki.spring5recipeapp.controllers;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +15,7 @@ import pl.iamkonradkrakowiecki.spring5recipeapp.services.RecipeService;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -86,5 +88,14 @@ public class RecipeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/recipeform"))
                 .andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
+    public void testDeleteAction() throws Exception {
+        mockMvc.perform(get("/recipe/1/delete"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+
+        Mockito.verify(recipeService, times(1)).deleteById(anyLong());
     }
 }
