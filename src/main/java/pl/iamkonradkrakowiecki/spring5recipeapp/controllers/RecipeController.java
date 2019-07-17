1fcb5065ace487a12d90a9567da1d6620e59cad2
konradkrakowiecki <konrad.krakowiecki@gmail.com>
2019-07-17 13:53:28 +0200

    Added MVC Exception Handler.

--- src/main/java/pl/iamkonradkrakowiecki/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/pl/iamkonradkrakowiecki/spring5recipeapp/controllers/RecipeController.java
@@ -1,15 +1,17 @@
 package pl.iamkonradkrakowiecki.spring5recipeapp.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import pl.iamkonradkrakowiecki.spring5recipeapp.commands.RecipeCommand;
 import pl.iamkonradkrakowiecki.spring5recipeapp.domain.Difficulty;
+import pl.iamkonradkrakowiecki.spring5recipeapp.exceptions.NotFoundException;
 import pl.iamkonradkrakowiecki.spring5recipeapp.services.RecipeService;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -52,10 +54,23 @@ public class RecipeController {
 
     @GetMapping("recipe/{id}/delete")
     public String deleteById(@PathVariable String id) {
-//        log.debug("Deleting id: " + id);
+        log.debug("Deleting id: " + id);
 
         recipeService.deleteById(Long.valueOf(id));
 
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
